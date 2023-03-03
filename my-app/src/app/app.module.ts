d9254a566ff9afef01284775811dc0a748fba437
gityoungjin <totore327@gmail.com>
2023-03-03 13:21:53 +0900

    ss

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MyComponentComponent } from './my-component/my-component.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyComponentComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
